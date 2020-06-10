11955588b07cb5c0d9c1e5540c35dc839b11abdb
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-10 14:25:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,9 @@ def load_library(file_path)
   #binding.pry
   final_result = emoticons.each_with_object({}) do |(key, value), final_hash|
     emoticons[key] = {:english => value[0], :japanese => value[1]}
+    
   end
+  binding.pry
   final_result
 end
 
