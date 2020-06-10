9f8b6e08d3044e06a6f23d98d57971d8bc07fa2e
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-10 14:18:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,14 @@
 # require modules here
+require 'pry'
 
-def load_library
+def load_library(file_path)
   # code goes here
+  require "yaml"
+  emoticons = YAML.load_file(file_path)
+  #binding.pry
+  emoticons.each_with_object do |(key, value), final_hash|
+    binding.pry
+  end
 end
 
 def get_japanese_emoticon
