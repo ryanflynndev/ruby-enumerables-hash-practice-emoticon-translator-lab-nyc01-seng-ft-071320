b8f736f709d45e038aebb6489749436a45d356db
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-10 14:47:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,8 @@ def get_english_meaning(file_path, emoticon)
     emoticons = load_library(file_path)
   emoticons.each do |key, value|
     if emoticons[key][:japanese] == emoticon 
-      return key 
+      return key
+    end
   end
   return "Sorry, that emoticon was not found"
 end
