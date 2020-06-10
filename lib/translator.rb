1bc7912a5e2e2ad0a926a1965edd6c28655dbf1d
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-10 14:18:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   require "yaml"
   emoticons = YAML.load_file(file_path)
   #binding.pry
-  emoticons.each_with_object do |(key, value), final_hash|
+  emoticons.each_with_object({}) do |(key, value), final_hash|
     binding.pry
   end
 end
